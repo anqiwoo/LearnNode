1f8b3a0f22d63214023c4025ab791ec237bb80c9
anqiwu <anqiwu85@outlook.com>
2022-05-23 10:15:12 +0800

    22/5/23 Node Fun

--- JoinUs/app.js
+++ JoinUs/app.js
@@ -1,5 +1,8 @@
-const { faker } = require('@faker-js/faker');
 const mysql = require('mysql');
+const express = require('express');
+var bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -8,11 +11,10 @@ var connection = mysql.createConnection({
     database: 'join_us'
 });
 
-const express = require('express');
-const app = express();
-const port = 3000;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
     // Find the count of users in DB
@@ -21,12 +23,18 @@ app.get('/', function(req, res) {
         if (err) throw err;
         var count = results[0].count;
         res.render('home', { count: count });
-        // var msg = "We have " + results[0].count + " users in our db!";
-        // res.send(msg);
-    });
-    // console.log(req);
-    // res.send('Hello World!');
+    }); // Put them within one parens -> Just to prevent callback hell!!!
 });
+
+app.post('/register', function(req, res) {
+    var person = { email: req.body.email };
+    connection.query('INSERT INTO users SET ?', person, function(err, results) {
+        if (err) throw err;
+        console.log(err);
+        console.log(results);
+        res.redirect('/');
+    });
+})
 
 app.get('/joke', function(req, res) {
     var joke = "What do you call a dog that does magic tricks? A labracadabrador.";
